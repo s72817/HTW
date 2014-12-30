7a51c2a1e2639691f06723fd74a637271c0482c6
s72817 <lambo_1@gmx.de>
2014-12-30 18:36:40 +0100

    Menu anfang

--- APL/APL/main.c
+++ APL/APL/main.c
@@ -4,7 +4,9 @@
 #include <math.h>
 
 //Eigene Dateien
+#include "menu.h"
 #include "funktionen.c"
+//#include "menu.c"
 
 #define MaxHash 10
 #define KN_len 10 //Anz der Ziffernn der Kontonummer
@@ -141,8 +143,6 @@ void kunde_delete(kunde *konto, long long int Kontonummer) {
 }
 
 
-
-
 void kunde_output_all(kunde *konto, kunde **hashtabelle) {
 	for (int i = 0; i < MaxHash; i++){
 		konto = hashtabelle[i];
@@ -165,33 +165,44 @@ void kunde_output_byhash(kunde *konto, long long int KN) {
 	}
 }
 
+
 int main() {
+	menu *mymenu = NULL; // init. die Liste mit NULL = leere liste
+
+	menu_add(&mymenu, "Kunde anlegen", 1);
+	menu_add(&mymenu, "Anmelden", 1);
+	menu_add(&mymenu, "Beenden", 1);
+	menu_add(&mymenu, "Einzahlung", 2);
+	menu_add(&mymenu, "Auszahlung", 2);
+	menu_add(&mymenu, "Ueberweisen", 2);
+	menu_add(&mymenu, "Kontostand", 2);
+	menu_add(&mymenu, "4-stellige PIN aendern", 2);
+	menu_add(&mymenu, "Kundendaten anzeigen", 2);
+	menu_add(&mymenu, "Konto loeschen", 2);
+	menu_add(&mymenu, "Abmelden", 2);
+	menu_add(&mymenu, "Beenden", 2);
+	menu_add(&mymenu, "Kunde suchen", 3);
+	menu_add(&mymenu, "Kontonummer mit gleichem Hashwert", 3);
+	menu_add(&mymenu, "zurueck", 3);
+
 	kunde *k = NULL; //komplette Liste
 	kunde me; //Aktuelles Object
 
 	//kunde *hashtable[MaxHash]; //Hash-Tabelle
 
 	int counter = 0;
+	int islogged = 0;
 
 
 	//Menu
 	int auswahl;
-	while (1) {
-		printf("1 - Kunde anlegen\n");
-		printf("2 - Kunde suchen\n");
-		printf("3 - Kunden unsortiert ausgeben\n");
-		printf("4 - Kunde loeschen\n");
-
+	while (1) { //1.Menuebene
+		menu_show(mymenu, 1); //Menu erzeugen
+		printf("\nIhre Wahl:\n");
 		scanf_s("%d", &auswahl);
 		switch (auswahl) {
-		case 1:
-			//kunde_add(&k, counter++);
-			for (int i = 0; i < 20; i++)
-				kunde_add(k, counter++);
-				
-			break;
-		case 2:
-		{
+		case 1: kunde_add(&k, counter++);
+		case 2: {
 			long long int tmp_KN;
 			printf("Kontonummer:");
 			scanf_s("%lld", &tmp_KN);
@@ -200,10 +211,17 @@ int main() {
 			if (digits(tmp_KN) == 10) {
 				int KN[KN_len];
 				num_to_arr(KN, KN_len, tmp_KN);
-				if (!check_acc_no(KN)) printf("Falsch\n");
+				if (!check_acc_no(KN)) printf("Falsch\n"); //Prüfziffer checken
 
 				if (kunde_suche(k, tmp_KN) != NULL) {
 					me = *kunde_suche(k, tmp_KN);
+					if (me.isblocked = 1) {
+						printf("\n\n----------------------------\n");
+						printf("Die Kontonummer %d ist blockiert.\nBitte wenden Sie sich an den naechsten\nfreien Service-Mitarbeiter.\n", me.Kontonummer);
+						printf("----------------------------\n");
+						break;
+					}
+					islogged = 1;
 					printf("Nummer %lld gefunden!\n", arr_to_num(me.Kontonummer, KN_len));
 				}
 				else
@@ -211,35 +229,76 @@ int main() {
 				break;
 			}
 			else printf("Bitte geben Sie ihre 10-stellige Kontonummer an!\n");
+			break;
+		}
+		default:
+			printf("Ihrer Eingabe konnte kein Menuepunkt zugeordnet werden!\nBitte versuchen Sie es erneut.\n");
+			break;
 			
-		}
-			break;
-		case 3: {
-			long long int tmp_KN;
-			printf("Kontonummer:");
-			scanf_s("%lld", &tmp_KN);
-			while (getchar() != '\n');
-			if (kunde_suche(k, tmp_KN) != NULL)
-				kunde_output_byhash(k, tmp_KN);
-			else printf("Kontonummer nicht gefunden!\n");
-			break;
-		}
-		case 4:{
-			long long int tmp_KN;
-			printf("Kontonummer:");
-			scanf_s("%lld", &tmp_KN);
-			if (kunde_suche(k, tmp_KN) != NULL)
-				kunde_delete(k, tmp_KN);
-			else printf("Kontonummer nicht gefunden!\n");
-		}
-		}
-	}
+			
+		} //Switch
+	} //1.Menu
 
-	//getch();
+	getch();
 	return 0;
 }
 
-
+//printf("1 - Kunde anlegen\n");
+//printf("2 - Kunde suchen\n");
+//printf("3 - Kunden unsortiert ausgeben\n");
+//printf("4 - Kunde loeschen\n");
+//
+//scanf_s("%d", &auswahl);
+//switch (auswahl) {
+//case 1:
+//	//kunde_add(&k, counter++);
+//	for (int i = 0; i < 20; i++)
+//		kunde_add(k, counter++);
+//
+//	break;
+//case 2:
+//{
+//	long long int tmp_KN;
+//	printf("Kontonummer:");
+//	scanf_s("%lld", &tmp_KN);
+//	while (getchar() != '\n');
+//
+//	if (digits(tmp_KN) == 10) {
+//		int KN[KN_len];
+//		num_to_arr(KN, KN_len, tmp_KN);
+//		if (!check_acc_no(KN)) printf("Falsch\n");
+//
+//		if (kunde_suche(k, tmp_KN) != NULL) {
+//			me = *kunde_suche(k, tmp_KN);
+//			printf("Nummer %lld gefunden!\n", arr_to_num(me.Kontonummer, KN_len));
+//		}
+//		else
+//			printf("Nummer nicht gefunden!\n");
+//		break;
+//	}
+//	else printf("Bitte geben Sie ihre 10-stellige Kontonummer an!\n");
+//
+//}
+//break;
+//case 3: {
+//	long long int tmp_KN;
+//	printf("Kontonummer:");
+//	scanf_s("%lld", &tmp_KN);
+//	while (getchar() != '\n');
+//	if (kunde_suche(k, tmp_KN) != NULL)
+//		kunde_output_byhash(k, tmp_KN);
+//	else printf("Kontonummer nicht gefunden!\n");
+//	break;
+//}
+//case 4: {
+//	long long int tmp_KN;
+//	printf("Kontonummer:");
+//	scanf_s("%lld", &tmp_KN);
+//	if (kunde_suche(k, tmp_KN) != NULL)
+//		kunde_delete(k, tmp_KN);
+//	else printf("Kontonummer nicht gefunden!\n");
+//}
+//}
 
 /// <summary>
 /// String_to_uls the specified string.
---
+++ APL/APL/menu.h
@@ -0,0 +1,57 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/// <summary>
+/// Menu-Struktur mit Ebenen
+/// </summary>
+/// <param name="item">Menu-Name</param>
+/// <param name="level">Menuebene</param>
+typedef struct _menu{
+	char item[40];
+	int level;
+	struct _menu *next;
+} menu;
+
+
+/// <summary>
+/// Menuelement hinzufugen
+/// </summary>
+/// <param name="list">Zeiger auf Liste</param>
+/// <param name="item">Menu-Name</param>
+/// <param name="level">Menuebene</param>
+void menu_add(menu **list, char *item, int level){
+
+	menu *newEl;
+	menu *lst_iter = *list;
+
+	/*char string[80];
+	strcpy_s(string, sizeof(string), "Hello world from ");*/
+
+	newEl = (menu*)malloc(sizeof(*newEl)); // erzeuge ein neues Element
+	strcpy_s(newEl->item, sizeof(newEl->item), item);
+	newEl->level = level;
+	newEl->next = NULL; // Wichtig für das Erkennen des Listenendes
+
+	if (lst_iter != NULL) { // sind Elemente vorhanden
+		while (lst_iter->next != NULL) // suche das letzte Element
+			lst_iter = lst_iter->next;
+		lst_iter->next = newEl; // Hänge das Element hinten an
+	}
+	else // wenn die liste leer ist, bin ich das erste Element
+		*list = newEl;
+}
+
+/// <summary>
+/// Menu wird erstellt
+/// </summary>
+/// <param name="list">Liste</param>
+/// <param name="level">Menuebene</param>
+void menu_show(menu *list, int level) {
+	int counter = 1;
+	while (list != NULL) {
+		if (list->level == level)
+			printf("%d. %s\n", counter++, list->item);
+		list = list->next;
+	}
+}
